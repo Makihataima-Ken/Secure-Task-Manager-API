01a50ac8278dbe25166d3b056b553016f4d6777c
Ken <ahmadsoud63@gmail.com>
2025-05-12 19:58:45 +0300

    update dtos to match validation

--- api/src/main/java/com/securetaskmanagerapi/api/dto/CreateTaskDTO.java
+++ api/src/main/java/com/securetaskmanagerapi/api/dto/CreateTaskDTO.java
@@ -2,7 +2,11 @@ package com.securetaskmanagerapi.api.dto;
 
 import java.time.LocalDate;
 
+import com.securetaskmanagerapi.api.entity.Status;
+
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,9 +15,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateTaskDTO {
-    @NotBlank
+    @NotBlank(message = "Title is required")
+    @Size(max = 100, message = "Title cannot exceed 100 characters")
     private String title;
 
+    @Size(max = 500, message = "Description cannot exceed 500 characters")
     private String description;
+
+    @FutureOrPresent(message = "Due date must be in the present or future")
     private LocalDate dueDate;
+
+    private Status status;
 }
--- api/src/main/java/com/securetaskmanagerapi/api/dto/UpdateTaskDTO.java
+++ api/src/main/java/com/securetaskmanagerapi/api/dto/UpdateTaskDTO.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 
 import com.securetaskmanagerapi.api.entity.Status;
 
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,8 +15,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateTaskDTO {
+    @Size(max = 100, message = "Title cannot exceed 100 characters")
     private String title;
+
+    @Size(max = 500, message = "Description cannot exceed 500 characters")
     private String description;
+
+    @FutureOrPresent(message = "Due date must be in the present or future")
     private LocalDate dueDate;
+
     private Status status;
 }
