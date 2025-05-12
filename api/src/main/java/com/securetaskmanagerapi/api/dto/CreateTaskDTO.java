f00de8ff46e11c93ca4bac9c1bc0f16f9281651d
Ken <ahmadsoud63@gmail.com>
2025-05-12 22:34:16 +0300

    add thymeleaf

--- api/src/main/java/com/securetaskmanagerapi/api/dto/CreateTaskDTO.java
+++ api/src/main/java/com/securetaskmanagerapi/api/dto/CreateTaskDTO.java
@@ -9,7 +9,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Builder.Default;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
